5db791aa36293ae7b3c79a2cf651ab702ed8651d
nerdDan <github@danc.co>
2014-10-30 13:58:04 +0800

    ~

--- HDU/4804.cpp
+++ HDU/4804.cpp
@@ -1,52 +1,66 @@
 #include<cstdio>
 #include<cstring>
 
-const int MOD = 1000000007;
-int h, w, upp, low;
+using namespace std;
+const int MOD = 1e9+7;
+
+int n, m, lo, hi;
 short a[101];
-long long b[2][1<<10][22];
+int f[4][1<<10][21];
+
+void cut(int& x) { if(x>=MOD) x -= MOD; }
+
+inline void bazinga0() {
+  memset(a, 0, sizeof a);
+  a[0] = (1<<m)-1;
+  for(int i = 1; i<=n; i++) {
+    getchar();
+    for(int j = 0; j<m; j++) {
+      char c = getchar();
+      if(c=='1')
+        a[i] ^= 1<<j;
+    }
+  }
+}
+
+inline void bazinga1() {
+  for(int j = 0; j<m; j++) {
+    memset(f[j&3], 0, sizeof f[0]);
+    f[j&3][(1<<(j+1))-1][0] = 1;
+  }
+  for(int i = 1; i<=n; i++)
+    for(int j = 0; j<m; j++) {
+      int _ = i*m+j;
+      memset(f[_&3], 0, sizeof f[0]);
+      for(int s = 0; s<1<<m; s++)
+        for(int k = 0; k<=hi; k++)
+          if(a[i]&(1<<j)) {                                  // 可放
+            if(s&(1<<j)) {                                   //   放了
+              if(k)                                          //     1x1
+                f[_&3][s][k] += f[(_-1)&3][s][k-1];
+              f[_&3][s][k] += f[(_-1)&3][s^(1<<j)][k];       //     2x1
+              cut(f[_&3][s][k]);
+              if(j && (a[i]&(1<<(j-1))) && (s&(1<<(j-1)))) { //     1x2
+                f[_&3][s][k] += f[(_-2)&3][s][k];
+                cut(f[_&3][s][k]);
+              }
+            } else {                                         //   沒放
+              f[_&3][s][k] = f[(_-1)&3][s^(1<<j)][k];
+            }
+          } else if(s&(1<<j)) {                              // 不可放
+            f[_&3][s][k] = f[(_-1)&3][s][k];
+          }
+    }
+}
 
 int main() {
-  while(~scanf("%d%d%d%d", &h, &w, &low, &upp)) {
-    bool s = 0, t = 1;
+  while(~scanf("%d%d%d%d", &n, &m, &lo, &hi)) {
+    bazinga0();
+    bazinga1();
     int res = 0;
-    memset(a, 0, sizeof(a));
-    a[0] = (1 << w)-1;
-    for(int i = 1; i <= h; i++) {
-      scanf("%*c");
-      for(int j = 0; j < w; j++) {
-        char t0;
-        scanf("%c", &t0);
-        a[i] |= (t0=='1')<<j;
-      }
-    }
-    memset(b[s], 0, sizeof(b[0]));
-    b[s][(1 << w) - 1][0] = 1;
-    for(int i = 1; i <= h; i++)
-      for(int j = 0; j < w; j++, s^=1, t^=1) {
-        memset(b[t], 0, sizeof(b[t]));
-        int t0 = a[i-1]>>j | (a[i] & ((1 << j) - 1))<<(w - j);
-        for(int x = t0; ; x = x-1&t0) {
-          for(int y = 0; y <= upp; y++)
-            if(b[s][x][y]) {
-              b[s][x][y] %= MOD;
-              if((x&1) == (a[i-1]>>j & 1)) {
-                b[t][x>>1][y] += b[s][x][y];
-                b[t][x>>1 | 1<<(w-1)][y+1] += b[s][x][y];
-                if(j && !(x & 1<<(w-1)))
-                  b[t][x>>1 | 3<<(w-2)][y] += b[s][x][y];
-              }
-              if(!(x&1) && (a[i-1]>>j & 1))
-                b[t][x>>1 | 1<<(w-1)][y] += b[s][x][y];
-            }
-          if(!x)
-            break;
-        }
-      }
-    for(int y = low; y <= upp; y++) {
-      b[s][a[h]][y] %= MOD;
-      res += b[s][a[h]][y];
-      res %= MOD;
+    for(int i = lo; i<=hi; i++) {
+      res += f[(n*m+m-1)&3][(1<<m)-1][i];
+      cut(res);
     }
     printf("%d\n", res);
   }
